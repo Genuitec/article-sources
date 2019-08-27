d4a662e93045f800a8ec4e6889e9c000eb54ccc8
mau253 <mau253@gmail.com>
2019-08-27 12:26:16 -0700

    First commit - Vue Firebase Upload

---
+++ vue-firebase-upload/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <upload></upload>
+  </div>
+</template>
+
+<script>
+import Upload from './components/Upload.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    Upload
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
